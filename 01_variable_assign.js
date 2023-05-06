1deb3ecd91fb744e76a0b88d518bd81b6b019cf8
devikapatgaonkar <devika.patgaonkar99@gmail.com>
2023-05-06 18:28:19 +0530

    java variable
    java variable

---
+++ 01_variable_assign.js
@@ -0,0 +1,23 @@
+var myfullName ;
+
+console.log(myfullName);
+fullName = "Devika Pradeep Patgaonkar";
+console.log(fullName);
+var myLove = "I Love js";
+console.log(myLove);
+var firstName = "Devika";
+console.log(firstName);
+var lastName = "Patgaonkar";
+console.log(lastName);
+var age ="24";
+console.log(age);
+var address = "Kothrud Pune";
+console.log(address);
+var pincode = "416416";
+console.log(pincode);
+var gender = "Female"
+console.log(gender);
+var isMarried = "No"
+console.log(isMarried);
+
+
---
+++ hello.js
@@ -0,0 +1,18 @@
+
+
+
+
+console.log("Devika Patgaonkar");
+
+ var firstName = "Devika";
+ console.log(firstName);
+
+ var age; // variable declaration
+ age ="22"; // variable intialisation
+ console.log(age);
+ var holder;
+ console.log(holder);//undefined
+ holder = "My spsec";
+ console.log(holder);
+ holder = "Sachin spaec";
+ console.log(holder);//variable update
