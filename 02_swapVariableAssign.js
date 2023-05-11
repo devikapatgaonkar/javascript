a75b065267e0697de12a9c8c655a06bc8e80c6d0
devikapatgaonkar <devika.patgaonkar99@gmail.com>
2023-05-11 13:20:46 +0530

    swapvariable
    swapvariable

---
+++ 02_swapVariableAssign.js
@@ -0,0 +1,22 @@
+
+console.log("******Assignment No 1******");
+var sweety = "Sweety";
+var cutie = "Cutie";
+console.log("Before swap:","sweety:",sweety,"cutie:",cutie);
+var temp = sweety;
+var sweety = cutie;
+var cutie = temp;
+console.log("After Swap:","sweety:",sweety,"cutie:",cutie);
+
+console.log("******Assignment No 2******");
+var num1 =100;
+var num2 =200;
+var num3 =300;
+console.log("Before Swap:","num1:",num1,"num2:",num2,"num3:",num3);
+var temp = num1;
+var num1 = num2;
+var num2 = num3;
+var num3 = temp;
+console.log("After Swap:","num1:",num1,"num2:",num2,"num3:",num3);
+
+
